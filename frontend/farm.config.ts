576185f3faec8785a3481279b05e46ed318e3fc4
苏向夜 <fu050409@163.com>
2025-04-17 09:59:50 +0800

    chore(deps): bump deps versions

--- frontend/farm.config.ts
+++ frontend/farm.config.ts
@@ -1,29 +1,27 @@
-import { defineConfig } from '@farmfe/core';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "@farmfe/core";
+import vue from "unplugin-vue/rollup";
+import path from "node:path";
 
 export default defineConfig({
 	vitePlugins: [vue()],
 	compilation: {
 		output: {
-			path: '../dist',
+			path: "../dist",
 		},
 		resolve: {
 			alias: {
 				"@": path.join(process.cwd(), "src"),
-			}
+			},
 		},
 	},
-	plugins: [
-		"@farmfe/plugin-sass"
-	],
+	plugins: ["@farmfe/plugin-sass"],
 	server: {
 		proxy: {
 			"/api": {
 				target: process.env.FARM_API_BASE_URL,
 				changeOrigin: true,
-				pathRewrite: (path: any) => path.replace(/^\/api/, ""),
-			}
-		}
-	}
+				pathRewrite: (path: string) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 });
